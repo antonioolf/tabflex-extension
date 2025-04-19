73133652c747700d045cfaee2ee4c9deaaf4bded
Antônio Oliveira <antonio.filho128@gmail.com>
2025-04-19 16:41:33 -0300

    actions deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/tabflex-extension/",
   plugins: [react()],
   build: {
     outDir: "dist",
