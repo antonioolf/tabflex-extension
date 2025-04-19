92479a948c970695506a60680155fb9ef876d38f
Antônio Oliveira <antonio.filho128@gmail.com>
2025-04-19 16:31:17 -0300

    checkpoint

--- src/components/NewTabPage/SearchBar.jsx
+++ src/components/NewTabPage/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { TextField, Paper, InputAdornment, IconButton } from "@mui/material";
+import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/material/styles";
 import React from "react";
@@ -21,51 +21,34 @@ const SearchBar = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     const query = e.target.search.value.trim();
-    if (query) {
-      window.open(
-        `https://www.google.com/search?q=${encodeURIComponent(query)}`,
-        "_blank"
-      );
-    }
+    window.open(
+      `https://www.google.com/search?q=${encodeURIComponent(query)}`,
+      "_self"
+    );
   };
 
   return (
     <SearchBarStyled onSubmit={handleSearch}>
-      <IconButton type="submit">
-        <SearchIcon />
-      </IconButton>
-      <input
-        type="text"
-        placeholder="Search Google or type a URL"
-        style={{
-          flex: 1,
-          border: "none",
-          outline: "none",
-          padding: "8px",
-          fontSize: "16px",
-        }}
-      />
+      <form onSubmit={handleSearch} style={{ display: "flex", width: "100%" }}>
+        <IconButton type="submit">
+          <SearchIcon />
+        </IconButton>
+        <input
+          name="search"
+          autoComplete="off"
+          autoFocus
+          type="search"
+          placeholder="Search Google or type a URL"
+          style={{
+            flex: 1,
+            border: "none",
+            outline: "none",
+            padding: "8px",
+            fontSize: "16px",
+          }}
+        />
+      </form>
     </SearchBarStyled>
-    // <Paper
-    //   component="form"
-    //   onSubmit={handleSearch}
-    //   sx={{ mb: 4, width: "40%" }}
-    // >
-    //   <TextField
-    //     fullWidth
-    //     name="search"
-    //     placeholder="Search Google or type a URL"
-    //     InputProps={{
-    //       startAdornment: (
-    //         <InputAdornment position="start">
-    //           <IconButton type="submit">
-    //             <SearchIcon />
-    //           </IconButton>
-    //         </InputAdornment>
-    //       ),
-    //     }}
-    //   />
-    // </Paper>
   );
 };
 
