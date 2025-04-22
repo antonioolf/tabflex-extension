b4065b98485b0cd9349f059fe5c6c86f30b2de9b
Antônio Oliveira <antonio.filho128@gmail.com>
2025-04-22 00:07:57 -0300

    fix deploy issue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/tabflex-extension/",
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: "./index.html",
+export default defineConfig(({ mode }) => {
+  const isGithubPages = mode === "github";
+
+  return {
+    base: isGithubPages ? "/tabflex-extension/" : "",
+    plugins: [react()],
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
       },
     },
-  },
+  };
 });
